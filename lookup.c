589b3969171b9442a96c3a5bff1d95a2748f30d1
Jose antonio Soriano sanchez <jsoriano@c2r5s6.42madrid.com>
2019-09-15 18:59:27 +0200

    cambios llokup

--- lookup.c
+++ lookup.c
@@ -1,43 +1,43 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lookup.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsoriano <jsoriano@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/15 16:38:13 by rpanader          #+#    #+#             */
+/*   Updated: 2019/09/15 18:35:44 by jsoriano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void lookup(int ***matriz, int nsol)
+int g_cont;
+int g_hmax;
+
+void	lookup(int ***matriz, int nsol)
 {
-    int n;
-    int col;
-    int row;
-    int cont;
-    int hmax;
-    
-    n = 1;
-    
-    col = 1;
-    row = 4;
-    hmax = 0;
-    cont = 0;
-    nsol = 577;
-    while(n <= nsol)
-    {
-        while(col < 5)
-        {
-            while(row > 0)
-            {
-                if (hmax < matriz[n][row][col])
-                {
-                    cont++;
-                    hmax = matriz[n][row][col];
-                }
-                row --;
-            }
-            matriz[n][5][col]= cont;
-            cont = 0;
-            hmax =0;
-            row = 4;
-            col++;
-        }
-        col = 1;
-        n++;
-    }
-    
-    }
+	int n;
+	int col;
+	int row;
+
+	n = 1;
+	col = 1;
+	row = 4;
+	g_hmax = 0;
+	while (n++ <= nsol)
+	{
+		g_cont = 0;
+		while (col++ < 5)
+			while (row-- > 0)
+			{
+				if (g_hmax < matriz[n][row][col])
+				{
+					g_cont++;
+					g_hmax = matriz[n][row][col];
+				}
+				matriz[n][5][col] = g_cont;
+				g_hmax = 0;
+				row = 4;
+			}
+		col = 1;
+	}
+}
