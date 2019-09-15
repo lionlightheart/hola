aa207b978d9fb1bb2e17b67f1276d1077ccd2f01
Daniel Perez zafra <dperez-z@c2r6s5.42madrid.com>
2019-09-15 19:24:31 +0200

    solve

--- solve.c
+++ solve.c
@@ -1,29 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solve.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dperez-z <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/15 19:18:44 by dperez-z          #+#    #+#             */
+/*   Updated: 2019/09/15 19:18:57 by dperez-z         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-int solve(int **test, int  **sol)
+int	solve(int **test, int **sol)
 {
-    int n;
-    int m;
-    int flag;
-    
-    n = 1;
-    m = 1;
-    while ( n < 578 )
-    {
-        flag = 1;
-        while (m <= 16)
-        {
-            if (sol[n][m] != test [1][m-1])
-                flag = 0;
-            m++;
-        }
-        if (flag == 1)
-        {
-            return(n-1);
-            
-        }
-        m = 1;
-        n++;
-    }
-    return (0);
+	int	n;
+	int	m;
+	int flag;
+
+	n = 1;
+	m = 1;
+	while (n < 578)
+	{
+		flag = 1;
+		while (m <= 16)
+		{
+			if (sol[n][m] != test[1][m - 1])
+				flag = 0;
+			m++;
+		}
+		if (flag == 1)
+		{
+			return (n - 1);
+		}
+		m = 1;
+		n++;
+	}
+	return (0);
 }
